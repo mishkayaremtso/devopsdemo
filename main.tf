58cdc1327375dc4445077e5916e2b28a1525d43e
mishkayaremtso <49525882+mishkayaremtso@users.noreply.github.com>
2020-04-18 20:32:26 +0300

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  default = "devops-test-274316"
-}
-provider "google" {
-  project     = var.project_name
-  region      = "us-central1"
-  zone        = "us-central1-c"
-  credentials = "${file("credentials.json")}"
-}
