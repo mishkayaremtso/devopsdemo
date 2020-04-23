97f38229bfa98bed7051597e3cc4854d59c94666
Myhailo Yaremtso <mykle1488@gmail.com>
2020-04-23 18:02:52 +0300

    creating two instances

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -22,3 +22,26 @@ resource "google_compute_instance" "default" {
   }
 
 }
+resource "google_compute_instance" "default1" {
+  name         = "test"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-b"
+  tags         = ["ssh", "http", "http-server"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20200317"
+    }
+  }
+
+  metadata = {
+    ssh-keys = "mykle1488:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+
+  network_interface {
+    network = "default"
+    access_config {}
+
+  }
+
+}
