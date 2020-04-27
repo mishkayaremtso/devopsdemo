544b5142ab7fc077eb47638c0d5bd20e19e3c962
Myhailo Yaremtso <mykle1488@gmail.com>
2020-04-27 16:30:18 +0300

    jenkins added

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -1,9 +1,9 @@
 
 resource "google_compute_instance" "default" {
-  name         = "test1"
+  name         = "slave1"
   machine_type = "n1-standard-1"
   zone         = "us-west1-b"
-  tags         = ["ssh", "http", "all", "http-server"]
+  tags         = ["http-server"]
   boot_disk {
     initialize_params {
       image = "ubuntu-1804-bionic-v20200317"
@@ -20,13 +20,38 @@ resource "google_compute_instance" "default" {
     access_config {}
 
   }
-
+  metadata_startup_script = "sudo apt update && sudo apt install -y openjdk-8-jre && sudo ufw allow 8080 && sudo ufw allow ssh && sudo ufw allow http && sudo ufw enable "
 }
 resource "google_compute_instance" "default1" {
-  name         = "test"
+  name         = "master"
   machine_type = "n1-standard-1"
   zone         = "us-west1-b"
-  tags         = ["ssh", "http", "all", "http-server"]
+  tags         = ["http-server"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20200317"
+    }
+  }
+
+  metadata = {
+    ssh-keys = "mykle1488:${file("~/.ssh/id_rsa.pub")}"
+
+  }
+  metadata_startup_script = "sudo apt update && sudo apt install -y openjdk-8-jre && sudo ufw allow 8080 && sudo ufw allow ssh && sudo ufw allow http && sudo ufw enable && sudo apt install -y docker.io && sudo docker pull jenkins/jenkins && sudo docker run -p 8080:8080 --name=jenkins-master -d jenkins/jenkins"
+
+  network_interface {
+    network = "default"
+    access_config {}
+
+  }
+
+
+}
+resource "google_compute_instance" "default2" {
+  name         = "jenkins-prod"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-b"
+  tags         = ["http-server"]
   boot_disk {
     initialize_params {
       image = "ubuntu-1804-bionic-v20200317"
@@ -42,6 +67,8 @@ resource "google_compute_instance" "default1" {
     network = "default"
     access_config {}
 
+
   }
 
+  metadata_startup_script = "sudo apt update && sudo apt install -y openjdk-8-jre && sudo ufw allow 8080 && sudo ufw allow ssh && sudo ufw allow http && sudo ufw enable "
 }
